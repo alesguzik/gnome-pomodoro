e63e1ed9df730613acc6214f8585869161acc81f
Ales Huzik <ahuzik@atlassian.com>
2021-01-05 19:13:25 +1100

    Fix logError

--- plugins/gnome/extension/utils.js
+++ plugins/gnome/extension/utils.js
@@ -23,7 +23,6 @@ const Signals = imports.signals;
 const Shell = imports.gi.Shell;
 
 const Main = imports.ui.main;
-const ExtensionSystem = imports.ui.extensionSystem;
 
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
 
@@ -201,7 +200,7 @@ function getFocusedWindowInfo() {
 
 
 function logError(message) {
-    ExtensionSystem.logExtensionError(Extension.metadata.uuid, message);
+    Main.extensionManager.logExtensionError(Extension.metadata.uuid, message);
 }
 
 
